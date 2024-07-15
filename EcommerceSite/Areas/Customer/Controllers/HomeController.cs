272532901fa6411d7439ca66cfdc3a391619166f
Daniel <captainhook.poor@gmail.com>
2024-07-15 23:57:51 +0200

    Setting up Identity in project

--- EcommerceSite/Areas/Customer/Controllers/HomeController.cs
+++ EcommerceSite/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Ecomm.DataAccess.Repository.IRepository;
 using Ecomm.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,24 @@ namespace EcommerceSite.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            this.unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
--- EcommerceSite/Program.cs
+++ EcommerceSite/Program.cs
@@ -2,6 +2,7 @@ using Ecomm.DataAccess.Data;
 using Ecomm.DataAccess.Repository;
 using Ecomm.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+// configure for razor pages
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -28,7 +33,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
